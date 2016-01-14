abb62eedf2e8298706cda8de3a62fcfbdd00cc23
jnnstt <jnnstt@gmail.com>
2016-01-14 11:16:38 +0100

    zajęcia

--- skrypt.js
+++ skrypt.js
@@ -1,3 +1,10 @@
 $(document).on('click', '.ma-efekt', function () {
     $(this).toggleClass('powieksz');
 });
+
+
+$(".fullsize").height(window.innerHeight);
+
+$(window).on("resize", function () {
+    $(".fullsize").height(window.innerHeight);
+})
